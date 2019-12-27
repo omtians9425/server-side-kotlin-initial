07ec1d7bace7d1bbfea2277ecf433f18c6e473ec
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-12-27 10:52:10 +0900

    initial commit

---
+++ src/test/kotlin/com/example/sample_serverside_kotlin/demo/DemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.sample_serverside_kotlin.demo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
