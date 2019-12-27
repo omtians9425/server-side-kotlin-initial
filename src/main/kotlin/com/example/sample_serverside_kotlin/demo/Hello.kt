de59e40cc2681026f3dad0c733c5d68f56f80de7
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2019-12-27 11:07:31 +0900

    change to @Controller with thymeleaf

--- src/main/kotlin/com/example/sample_serverside_kotlin/demo/Hello.kt
+++ src/main/kotlin/com/example/sample_serverside_kotlin/demo/Hello.kt
@@ -1,16 +1,27 @@
 package com.example.sample_serverside_kotlin.demo
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
 /**
  * RestController : for API
  * Controller : for HTML
  */
-@RestController
+@Controller
 class Hello {
-    @RequestMapping("/")
-    fun index(): String {
-        return "Hello World!"
+
+    // for html
+    @GetMapping("/")
+    fun index(model: Model): String {
+        model.addAttribute("Greeting", "Hello World!") // name to value
+        return "index"
     }
+
+    // for REST
+//    @RequestMapping("/")
+//    fun index(): String {
+//        return "Hello World!"
+//    }
 }
